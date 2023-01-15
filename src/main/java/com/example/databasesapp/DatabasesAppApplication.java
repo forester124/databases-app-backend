8da121d797f6a08338531a7688dd535821c5357e
Damian <damian.skarbiecki@gmail.com>
2023-01-15 17:41:55 +0100

    commit

---
+++ src/main/java/com/example/databasesapp/DatabasesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.databasesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabasesAppApplication.class, args);
+    }
+
+}
